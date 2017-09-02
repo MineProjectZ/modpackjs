dbb02e5d0e646578884d7f5c78d642b1cd93ec1f
tilera <ttlp.craft@gmail.com>
2017-09-02 23:04:41 +0200

    1.2

---
+++ 1.2/examples/modpackjsonreader/README.md
@@ -0,0 +1,7 @@
+<pre>
+#How to Use
+-install nodejs
+-place the modpack.json in this folder
+-execute the createmodlist.bat(on Windows) or the createmodlist.sh(on Linux or Mac)
+The script will create a html and a txt list of all mods in the modpack.json.
+</pre>
---
+++ 1.2/examples/modpackjsonreader/index.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var file = fs.readFileSync("modpack.json", "utf8");
+var modpack = JSON.parse(file);
+fs.writeFile("./modlist.html", "Modlist <br>", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("created .HTML");
+});
+fs.writeFile("./modlist.txt", "Modlist", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("created .TXT");
+}); 
+var type = "";
+var version = "";
+switch (modpack.modpackjson.version) {
+    case "1.0":
+        version = true;
+        break;
+    case "1.1":
+        version = true;
+        break;
+    case "1.2":
+        version = true;
+        break;
+    default:
+        version = false;
+        break;
+}
+switch (modpack.modpackjson.type) {
+    case "modpack":
+        type = true;
+        break;
+    default:
+        type = false;
+        break;
+}
+if (version && type) {
+    modpack.dependencies.mods.forEach(function(object) {
+        var name = object.name;
+        fs.appendFile('./modlist.html', " \
+    " + name + "<br>", function (err) {
+            if (err) throw err;
+            console.log(name + 'added to .HTML');
+          });
+        fs.appendFile('./modlist.txt', " \
+    " + name, function (err) {
+            if (err) throw err;
+            console.log(name + 'added to .TXT');
+          });
+    });
+}else {
+    if (version){
+        console.log("This is not a Modpack!")
+    }else if (modpack){
+        console.log("This reader needs modpackJSON version 1.0, 1.1 or 1.2!")
+    }
+}
+
+
+
